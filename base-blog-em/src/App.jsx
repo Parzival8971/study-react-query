dbd3dabd2d84fcca1978f92b976f585603797c1c
Parzival8971 <esh8971@gmail.com>
2022-11-28 18:45:24 +0900

    QueryClientProvider 생성한다

--- base-blog-em/src/App.jsx
+++ base-blog-em/src/App.jsx
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+
+import { Posts } from './Posts';
+
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // provide React Query client to App
+    <QueryClientProvider client={queryClient}>
+      <div className='App'>
+        <h1>Blog Posts</h1>
+        <Posts />
+      </div>
+    </QueryClientProvider>
   );
 }
 
