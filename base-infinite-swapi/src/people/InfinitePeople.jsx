cee9afc857a0aec2b8e8913127725a75e9b5b864
Parzival8971 <esh8971@gmail.com>
2022-12-01 13:49:20 +0900

    useInfiniteQuery 호출 설정한다

--- base-infinite-swapi/src/people/InfinitePeople.jsx
+++ base-infinite-swapi/src/people/InfinitePeople.jsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import InfiniteScroll from 'react-infinite-scroller';
-import { Person } from './Person';
+import { useInfiniteQuery } from 'react-query';
 
+import { Person } from './Person';
 const initialUrl = 'https://swapi.dev/api/people/';
 const fetchUrl = async (url) => {
   const response = await axios.get(url);
@@ -9,6 +10,13 @@ const fetchUrl = async (url) => {
 };
 
 export function InfinitePeople() {
+  const { data, fetchNextPage, hasNextPage } = useInfiniteQuery(
+    'sw-people',
+    ({ pageParam = initialUrl }) => initialUrl(pageParam),
+    {
+      getNextPageParam: (lastPage) => lastPage.next || undefined,
+    }
+  );
   // TODO: get data for InfiniteScroll via React Query
   return <InfiniteScroll />;
 }
